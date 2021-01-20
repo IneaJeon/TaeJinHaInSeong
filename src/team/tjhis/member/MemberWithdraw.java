85f5f8cbc20d68acfa3fbe2a5abe123faef293b5
JH <yoojh0219@gmail.com>
2021-01-21 02:29:39 +0900

    20210121_유진희
    withdraw 메소드 선언

--- src/team/tjhis/member/MemberWithdraw.java
+++ src/team/tjhis/member/MemberWithdraw.java
@@ -3,5 +3,12 @@ package team.tjhis.member;
 public class MemberWithdraw {
 
 	// 회원탈퇴 메소드
-	
+	public MemberDTO[] withdraw(MemberDTO[] members, int no) {
+
+		
+		
+		
+		
+		return members; 
+	}
 }
