537ce188843ec8449be9019720bcf60cebc1b9dd
xogns <xogns@DESKTOP-UEO7RL6>
2021-01-21 17:35:34 +0900

    202101211734_김태훈
    회원정보수정 코드추가

--- src/team/tjhis/member/FindId.java
+++ src/team/tjhis/member/FindId.java
@@ -18,11 +18,11 @@ public class FindId {
 		System.out.print("휴대폰번호를 입력해주세요 : ");
 		String phoneNum = sc.nextLine();
 		
-//		if(name.equals(memberDto.getName) && phoneNum.equals(memberDto.getPhoneNum)) {
-//			System.out.println(name + "회원님의 ID는 " + memberDto.getId + "입니다.");
-//		} else {
-//			System.out.println("일치하는 회원의 정보가 없습니다.");
-//		}
+		if(name.equals(memberDto.getName()) && phoneNum.equals(memberDto.getPhoneNum())) {
+			System.out.println(name + "회원님의 ID는 " + memberDto.getId() + "입니다.");
+		} else {
+			System.out.println("일치하는 회원의 정보가 없습니다.");
+		}
 		
 	}
 	
--- src/team/tjhis/member/MyDataAmend.java
+++ src/team/tjhis/member/MyDataAmend.java
@@ -6,7 +6,7 @@ public class MyDataAmend {
 
 	public void amend() {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		
 		MemberDTO memberDto = new MemberDTO();
 		
@@ -14,35 +14,49 @@ public class MyDataAmend {
 		System.out.println("===== 회원정보수정 =====");
 		System.out.println("1. 비밀번호 수정");
 		System.out.println("2. 휴대폰번호 수정");
+		System.out.println("3. 주소 수정");
 		System.out.println("9. 회원탈퇴");
 		System.out.print("원하시는 메뉴의 번호를 입력해주세요 : ");
 		int num = sc.nextInt();
 		
-//		switch (num) {
-//			case 1:
-//				System.out.print("새로 설정할 비밀번호를 입력해주세요 : ");
-//				String pwd = sc.nextLine();
-//				System.out.println("비밀번호가 " + pwd + "로 변경되었습니다. ");
-//				memberDto.setPwd(pwd);
-//				break;
-//			case 2:
-//				System.out.print("새로 설정할 휴대폰번호를 입력해주세요 : ");
-//				String phoneNum = sc.nextLine();
-//				System.out.println("휴대폰번호가 " + phoneNum + "로 변경되었습니다. ");
-//				memberDto.setPhoneNum(phoneNum);
-//				break;
-//			case 9:
-//				System.out.println("정말로 탈퇴 하시겠습니까? [y/n] : ");
-//				int check = sc.next().charAt(0);
-//				if (check == 'y') {
-//					System.out.println("탈퇴가 완료되었습니다. 그 동안 감사했습니다.");
-//				} else {
-//					System.out.println("메뉴 선택으로 돌아갑니다.");
-//				}
-//				return;
-//			default:
-//				System.out.println("번호를 잘못입력하셨습니다. 다시 입력해주세요.");
-//			}
+		switch (num) {
+			case 1:
+				System.out.print("변경할 비밀번호를 입력해주세요 : ");
+				String pwd = sc.nextLine();
+				System.out.println("비밀번호가 " + pwd + "로 변경되었습니다. ");
+				memberDto.setPwd(pwd);
+				break;
+			case 2:
+				System.out.print("변경할 휴대폰번호를 입력해주세요 : ");
+				String phoneNum = sc.nextLine();
+				System.out.println("휴대폰번호가 " + phoneNum + "로 변경되었습니다. ");
+				memberDto.setPhoneNum(phoneNum);
+				break;
+			case 3:
+				System.out.print("변경할 주소를 입력해주세요 : ");
+				String addr = sc.nextLine();
+				System.out.println("주소가 " + addr + "로 변경되었습니다. ");
+				memberDto.setAddr(addr);
+				break;
+			case 9:
+//				MemberWithdraw.withdraw(?????????);
+				System.out.println("정말로 탈퇴 하시겠습니까? [y/n] : ");
+				int check = sc.next().charAt(0);
+				if (check == 'y') {
+					memberDto.setNo(0);
+					memberDto.setId(null);
+					memberDto.setPwd(null);
+					memberDto.setName(null);
+					memberDto.setPhoneNum(null);
+					memberDto.setAddr(null);
+					System.out.println("탈퇴가 완료되었습니다. 그 동안 감사했습니다.");
+				} else {
+					System.out.println("메뉴 선택으로 돌아갑니다.");
+				}
+				return;
+			default:
+				System.out.println("번호를 잘못입력하셨습니다. 다시 입력해주세요.");
+			}
 		}
 		
 	}
