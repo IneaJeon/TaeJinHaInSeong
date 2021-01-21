c4c29900da0636d13a4c51223859e1d8569a3b2b
JH <yoojh0219@gmail.com>
2021-01-21 23:49:09 +0900

    20210121_유진희
    코드 살리기

--- src/team/tjhis/member/MemberWithdraw.java
+++ src/team/tjhis/member/MemberWithdraw.java
@@ -3,11 +3,8 @@ package team.tjhis.member;
 public class MemberWithdraw {
 
 	// 회원탈퇴 메소드
-	public MemberDTO[] withdraw(MemberDTO[] members, int no) {
-	
-		members[no-1] = null;
-				 
-		return members; 
+	public void withdraw(MemberDTO[] db, int no) {
+		db[no-1] = null;	// 회원번호에 해당하는 회원의 정보 삭제
 	}	
 
 }
