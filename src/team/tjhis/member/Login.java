ccb01d6328592f3541146d210d7f06306f980bab
winze <winze@INEA>
2021-01-23 00:42:53 +0900

    20210123 전인애
    기존 로그인 페이지에 ArrayList 추가 했습니다.

--- src/team/tjhis/member/Login.java
+++ src/team/tjhis/member/Login.java
@@ -1,7 +1,51 @@
 package team.tjhis.member;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Login {
 
-	// 로그인 찾기 메소드
-	
-}
+	public void login(String id, String pwd) {
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> arr = new ArrayList<>();
+		MemberDTO memberDto = new MemberDTO();
+		
+		System.out.println("로그인");
+
+		while(true)  {
+			
+			int saw = sc.nextInt();
+			
+			/*아이디 입력*/
+			if(saw == 1) {
+				System.out.print("아이디 입력 하세요 : ");
+				id = sc.nextLine();
+				arr.add(id);
+			}else if(saw == 2){
+				System.out.print("아이디 입력 하세요 : ");
+				id = sc.nextLine();
+				if(arr.contains(id)) {
+					System.out.println("존재하는 아이디 입니다.");
+				}
+			}else if(saw == 3) {
+				System.out.print("아이디 입력 하세요 : ");
+				id = sc.nextLine();
+				if(arr.contains(id)) {
+					for(int i = 0; i< arr.size(); i++) {
+						if(arr.get(i).equals(id)) {
+							arr.remove(i);
+							break;
+						}
+					}
+				}else {
+					System.out.println("존재하지 않는 ID입니다.");
+				}
+			}else {
+				break;
+			}
+			System.out.println(arr);
+
+		}//while
+	}//login
+}//Login
