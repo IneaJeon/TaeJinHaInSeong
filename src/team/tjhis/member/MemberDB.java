128fb2502446b4118663e85473013df1bf8b1907
HY <HY@localhost>
2021-01-23 03:42:46 +0900

    20210123_민하영
    탈퇴한 회원 수 저장할 변수 wdNum 선언
    (진희님과 같이 코드리뷰)
    MemberDB 객체 배열 대신 ArrayList 활용
    getter & setter 생성

--- src/team/tjhis/member/MemberDB.java
+++ src/team/tjhis/member/MemberDB.java
@@ -1,8 +1,21 @@
 package team.tjhis.member;
 
+import java.util.ArrayList;
+
 public class MemberDB {
 	
-	// MemberDTO 배열 인스턴스
-	MemberDTO[] memberDB = new MemberDTO[100];
+	// index = no - 1 - wdNum;
+	private int wdNum;						 	// 탈퇴한 회원 수 (회원번호를 가지고 인덱스에 접근할 때 사용)
+	
+	ArrayList memberDB = new ArrayList();		// 객체 배열 대신 ArrayList 활용
+
+	public int getWdNum() {
+		return wdNum;
+	}
+
+	public void setWdNum(int wdNum) {
+		this.wdNum = wdNum;
+	}	
+	
 
 }
