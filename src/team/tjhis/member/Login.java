b7d3c9eb97b755c19ed62f190772326be48f0c0e
winze <winze@INEA>
2021-01-23 17:03:05 +0900

    20210123_전인애
    4차 수정

--- src/team/tjhis/member/Login.java
+++ src/team/tjhis/member/Login.java
@@ -5,28 +5,25 @@ import java.util.Scanner;
 public class Login {
 
 	public void login(MemberDB db) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
-		while (true) {
-			System.out.println("로그인");
-			System.out.println("ID 를 입력하세요 :");
-			String id = sc.nextLine();
-			for (int i = 0; i < db.memberDB.size(); i++) {
-				if (! db.memberDB.get(i).getId().equals(id)) {
-					System.out.println("잘못된 아이디 입니다.");
-				} else {
-					System.out.println("비밀번호를 입력하세요");
-					String pwd = sc.nextLine();
-					if (! db.memberDB.get(i).getPwd().equals(pwd)) {
-						System.out.println("비밀번호를 다시 확인하세요");
-					} else {
-						System.out.println("로그인 성공");
-						return;
-					}
-				}
+		System.out.println("로그인");
+		System.out.println("ID 를 입력하세요 :");
+		String id = sc.nextLine();
 
-			} // for
-		} // while
+		System.out.println("비밀번호를 입력하세요");
+		String pwd = sc.nextLine();
+
+		for (int i = 0; i < db.memberDB.size(); i++) {
+			if (db.memberDB.get(i).getId().equals(id) && db.memberDB.get(i).getPwd().equals(pwd)) {
+				System.out.println("로그인 성공");
+				break;
+			} else if(!db.memberDB.get(i).getId().equals(id)) {
+				System.out.println("입력하신 아이디가 존재 하지 않습니다.");
+			} else if(!db.memberDB.get(i).getPwd().equals(pwd)) {
+				System.out.println("입력하신 비밀번호가 존재 하지 않습니다.");
+			}
+		} // for
 	}// login
 }// Login
