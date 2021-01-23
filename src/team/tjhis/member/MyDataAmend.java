c116cfc24ed56a4926d2b48e0e24579385c1c80a
xogns <xogns@DESKTOP-UEO7RL6>
2021-01-23 13:19:16 +0900

    202101231319_김태훈
    반복문 빠져나올 수 있도록 수정

--- src/team/tjhis/member/MyDataAmend.java
+++ src/team/tjhis/member/MyDataAmend.java
@@ -15,6 +15,7 @@ public class MyDataAmend {
 			System.out.println("1. 비밀번호 수정");
 			System.out.println("2. 휴대폰번호 수정");
 			System.out.println("3. 주소 수정");
+			System.out.println("9. 메인으로 돌아가기");
 			System.out.print("원하시는 메뉴의 번호를 입력해주세요 : ");
 			int num = sc.nextInt();
 
@@ -31,23 +32,26 @@ public class MyDataAmend {
 							String changePwd = sc.nextLine();
 							db.memberDB.get(i).setPwd(pwd);
 							System.out.println("비밀번호가 " + changePwd + "로 변경되었습니다. ");
-							break;
+							return;
 						} else {
 							System.out.println("현재 비밀번호를 잘못 입력하셨습니다.");
+							break;
 						}
-						break;
 					case 2:
 						System.out.print("변경할 휴대폰번호를 입력해주세요 : ");
 						String changePhoneNum = sc.nextLine();
 						System.out.println("휴대폰번호가 " + changePhoneNum + "로 변경되었습니다. ");
 						db.memberDB.get(i).setPhoneNum(changePhoneNum);
-						break;
+						return;
 					case 3:
 						System.out.print("변경할 주소를 입력해주세요 : ");
 						String changeAddr = sc.nextLine();
 						System.out.println("주소가 " + changeAddr + "로 변경되었습니다. ");
 						db.memberDB.get(i).setAddr(changeAddr);
-						break;
+						return;
+					case 9:
+						System.out.println("메인 메뉴로 돌아갑니다.");
+						return;
 					default:
 						System.out.println("번호를 잘못입력하셨습니다. 다시 입력해주세요.");
 						break;
