ec9545edbcf3de8fd559b6edb3030c3c4af141d6
winze <winze@INEA>
2021-01-24 16:16:01 +0900

    20210124_전인애
    1. 회원가입 수정
    2. 로그인 수정

--- src/team/tjhis/member/SignUpManager.java
+++ src/team/tjhis/member/SignUpManager.java
@@ -4,54 +4,52 @@ import java.util.Scanner;
 
 public class SignUpManager {
 
-	public static void main(String[] args) {
-
-		/* navi */
-		System.out.println("Welcome to 저기요!");
-		System.out.println("step1. 아이디입력 \nstep2. 비밀번호 입력 \nstep3. 이름입력  \nstep4. 주소입력");
-		System.out.println();
-		
-		String[] list = {"win","홍길동", "서울시 서초구"};
-		int[] num = {123, 01234};
+	public void signUpStart(MemberDB db) {
 
 		Scanner sc = new Scanner(System.in);
 
-		/**/
-		Member m = new Member();
-
+		/* 로그인 시도 후 회원정보가 없다는 가정 하에 회원가입 진행 */
 		while (true) {
-			System.out.print("id를 입력하세요 : ");
-			m.id = sc.nextLine();
-			System.out.print("비밀번호를 입력하세요 : ");
-			m.pwd = sc.nextInt();
-			System.out.println("이름을 입력하세요 : ");
-			m.name = sc.nextLine();
-			System.out.println("주소를 입력하세요 : ");
-			m.adress = sc.nextLine();			
-			System.out.println("주소를 입력하세요 : ");
-			m.phone = sc.nextInt();
-			
-			if(!list[0].equals(m.id)) {
-				System.out.println("아이디가 다시");
-			}else if(!list[1].equals(m.pwd)) {
-				System.out.println("비번 다시");
-			}else if(!list[2].equals(m.name)) {
-				System.out.println("이름 다시");
-			}else if(num[0] != m.pwd) {
-				System.out.println("전화번호 다시");
-			}else {
-				System.out.println("회원가입 성공");
+			System.out.println("1. 로그인: ");
+			System.out.print("2. 회원가입 : ");
+			System.out.print("3. 종료 ");
+			int num = sc.nextInt();
+
+			if (num == 0) {
+				System.out.println("회원가입부터 해주세요.");
+				continue;
+			} else {
+				for (int i = 0; i < db.memberDB.size(); i++) {
+					/* 로그인 성공 회원가입 필요없음 */
+					if (num == 1) {
+						System.out.print("아이디 입력: ");
+						String id = sc.nextLine();
+						System.out.print("비밀번호 입력 : ");
+						String pwd = sc.nextLine();
+						if (db.memberDB.get(i).getId().equals(id) && db.memberDB.get(i).getPwd().equals(pwd)) {
+							System.out.println("로그인 성공");
+						} else {
+							System.out.println("다시 입력해 주세요");
+						}
+					/* 회원 정보 없을 시 회원가입 시작 */
+					} else if (num == 2) {
+						System.out.print("아이디 입력: ");
+						String id = sc.nextLine();
+						System.out.print("비밀번호 입력 : ");
+						String pwd = sc.nextLine();
+						System.out.print("이름를 입력하세요 : ");
+						String inputName = sc.nextLine();
+						System.out.print("전화번호 입력 : ");
+						String inputPhoneNum = sc.nextLine();
+						System.out.print("주소 입력 : ");
+						System.out.println("회원가입을 축하드립니다.");
+					/* 종료 */
+					} else if (num == 3) {
+						System.out.println("프로그램 종료");
+						break;
+					}
+				}
 			}
-
-		} 
-		
-		}//main
-	
-	}//SignUp
-
-
-class Member {
-	String id, name, adress;
-	int pwd, phone;
-
-}
+		} // while
+	}// signUpStart
+}// SignUpManager
