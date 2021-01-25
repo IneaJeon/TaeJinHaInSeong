db610898b57be77b4e96d6a52465093a13dc1ca1
HY <hymin1224@naver.com>
2021-01-26 02:24:25 +0900

    20210126_민하영
    MyPageManager - index 찾는 반복문 추가 및 회원 정보 출력문 추가

--- src/team/tjhis/member/MyPageManager.java
+++ src/team/tjhis/member/MyPageManager.java
@@ -14,9 +14,23 @@ public class MyPageManager {
 //		MyDataAmend amend = MyDataAmend.getInstance();				 
 		MemberWithdraw withdraw = MemberWithdraw.getInstance();
 		
+		int index = 0;
+		for(int i = 0 ; i < db.memberDB.size() ; i++) {
+			if(db.getLogNo() == db.memberDB.get(i).getNo()) {
+				index = i;
+				break;
+			}
+			
+		}
 		while(true) {
 			System.out.println();
 			System.out.println("__________ 마이 페이지 화면 __________");
+			System.out.println();
+			System.out.println("회원번호	[ " + db.memberDB.get(index).getNo() + " ]");
+			System.out.println("아이디	[ " + db.memberDB.get(index).getId() + " ]");
+			System.out.println("이름	[ " + db.memberDB.get(index).getName() + " ]");
+			System.out.println("휴대폰	[ " + db.memberDB.get(index).getPhoneNum() + " ]");
+			System.out.println("주소	[ " + db.memberDB.get(index).getAddr() + " ]");
 			System.out.println();
 			System.out.println("[ 1. 회원정보수정 ]");
 			System.out.println("[ 2. 탈퇴하기 ] ");
