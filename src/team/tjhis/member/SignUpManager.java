c6d640ebdb469654c9dd9fd53cb0e3b518921715
tj <tj@DESKTOP-MOB8F55>
2021-01-26 17:11:44 +0900

    20210126_전인애
    회원가입 수정

--- src/team/tjhis/member/SignUpManager.java
+++ src/team/tjhis/member/SignUpManager.java
@@ -4,52 +4,52 @@ import java.util.Scanner;
 
 public class SignUpManager {
 
-	public void signUpStart(MemberDB db) {
+	public void signUpStart(MemberDB db, int i) {
 
 		Scanner sc = new Scanner(System.in);
 
-		/* 로그인 시도 후 회원정보가 없다는 가정 하에 회원가입 진행 */
-		while (true) {
-			System.out.println("1. 로그인: ");
-			System.out.print("2. 회원가입 : ");
-			System.out.print("3. 종료 ");
-			int num = sc.nextInt();
+		System.out.println("회원가입");
 
-			if (num == 0) {
-				System.out.println("회원가입부터 해주세요.");
+		System.out.println("아이디 입력 :");
+		String id = sc.nextLine();
+		System.out.println("비밀번호 입력 :");
+		String pwd = sc.nextLine();
+		System.out.println("이름 입력 :");
+		String name = sc.nextLine();
+		System.out.println("전화번호 입력 :");
+		String phoneNum = sc.nextLine();
+		System.out.println("주소 입력 :");
+		String addr = sc.nextLine();
+
+		do {
+			/* 세팅하기 */
+//			MemberDTO md = new MemberDTO();
+//			db.memberDB.get(i).setNo(no);
+			db.memberDB.get(i).setId(id);
+			db.memberDB.get(i).setPwd(pwd);
+			db.memberDB.get(i).setPhoneNum(phoneNum);
+			db.memberDB.get(i).setAddr(addr);
+
+//			db.memberDB.add(md);
+
+			System.out.println("계속 진행 하시겠습니까?");
+			String yn = sc.nextLine();
+
+			if (yn.equals("y") || yn.equals("Y")) {
 				continue;
 			} else {
-				for (int i = 0; i < db.memberDB.size(); i++) {
-					/* 로그인 성공 회원가입 필요없음 */
-					if (num == 1) {
-						System.out.print("아이디 입력: ");
-						String id = sc.nextLine();
-						System.out.print("비밀번호 입력 : ");
-						String pwd = sc.nextLine();
-						if (db.memberDB.get(i).getId().equals(id) && db.memberDB.get(i).getPwd().equals(pwd)) {
-							System.out.println("로그인 성공");
-						} else {
-							System.out.println("다시 입력해 주세요");
-						}
-					/* 회원 정보 없을 시 회원가입 시작 */
-					} else if (num == 2) {
-						System.out.print("아이디 입력: ");
-						String id = sc.nextLine();
-						System.out.print("비밀번호 입력 : ");
-						String pwd = sc.nextLine();
-						System.out.print("이름를 입력하세요 : ");
-						String inputName = sc.nextLine();
-						System.out.print("전화번호 입력 : ");
-						String inputPhoneNum = sc.nextLine();
-						System.out.print("주소 입력 : ");
-						System.out.println("회원가입을 축하드립니다.");
-					/* 종료 */
-					} else if (num == 3) {
-						System.out.println("프로그램 종료");
-						break;
-					}
-				}
+				break;
 			}
-		} // while
+		} while (true);
+		
+		for (int i1 = 0; i1 < db.memberDB.size(); i1++) {
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getNo()); // 해당 데이터 출력
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getId()); // 해당 데이터 출력
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getPwd()); // 해당 데이터 출력
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getName()); // 해당 데이터 출력
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getPhoneNum()); // 해당 데이터 출력
+			System.out.println("회원 번호 : " + db.memberDB.get(i1).getAddr()); // 해당 데이터 출력
+
+		}
 	}// signUpStart
 }// SignUpManager
