3b3d3aa3868893b7934384d307f8471d219071d8
JH <yoojh0219@gmail.com>
2021-01-27 19:02:56 +0900

    20210127_유진희

--- src/team/tjhis/order/ChineseFood.java
+++ src/team/tjhis/order/ChineseFood.java
@@ -1,5 +1,42 @@
 package team.tjhis.order;
 
-public class ChineseFood {
+import java.util.Scanner;
 
+public class ChineseFood extends Food {
+
+	// 기본 생성자
+	public ChineseFood() {
+		super();
+	}
+	
+	public void orderChineseFood() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("__________강남반점__________");
+		System.out.println();
+		System.out.println("[ 1. 짜장면 ]");
+		System.out.println("[ 2. 짬뽕 ]");
+		System.out.println("[ 3. 탕수육 ]");
+		System.out.println("[ 9. 돌아가기 ]");
+		System.out.println("__________________________");
+		System.out.println();
+		System.out.print("메뉴를 선택해 주세요 : ");
+		int choice = sc.nextInt();
+		sc.nextLine();
+		
+		switch(choice) {
+		
+			case 1 : break;
+			case 2 : break;
+			case 3 : break;
+			case 9 : break;
+			default :
+			
+		}
+	
+	
+	
+	}
 }
