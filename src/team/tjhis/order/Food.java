65bf0ece2c6e125901bb8f308caf4c213bcb508b
good321ok@naver.com <good321ok@naver.com>
2021-01-27 17:48:15 +0900

    20210127_김성배
    푸드클래스 작성
    분식클래스 작성
    멤버패키지의 메인클래스
    오더매니저 호출 케이스 추가

--- src/team/tjhis/order/Food.java
+++ src/team/tjhis/order/Food.java
@@ -2,4 +2,56 @@ package team.tjhis.order;
 
 public class Food {
 
+	private String foodName;
+	private int price;
+	private int calorie;
+	private String countryOfOrigin;
+	
+	public Food() {}
+
+	public Food(String foodName, int price, int calorie, String countryOfOrigin) {
+		this.foodName = foodName;
+		this.price = price;
+		this.calorie = calorie;
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCalorie() {
+		return calorie;
+	}
+
+	public void setCalorie(int calorie) {
+		this.calorie = calorie;
+	}
+
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	@Override
+	public String toString() {
+		return "Food [foodName=" + foodName + ", price=" + price + ", calorie=" + calorie + ", countryOfOrigin="
+				+ countryOfOrigin + "]";
+	}
+	
 }
--- src/team/tjhis/order/KoreanFood.java
+++ src/team/tjhis/order/KoreanFood.java
@@ -1,5 +1,49 @@
 package team.tjhis.order;
 
-public class KoreanFood {
+public class KoreanFood extends Food {
+	
+	public KoreanFood() {}
+	
+	public KoreanFood(String foodName, int price, int calorie, String countryOfOrigin) {
+		super(foodName, price, calorie, countryOfOrigin);
+	}
+	
+	public String getFoodName() {
+		return super.getFoodName();
+	}
+
+	public void setFoodName(String foodName) {
+		super.setFoodName(foodName);
+	}
+	
+	public int getPrice() {
+		return super.getPrice();
+	}
+	
+	public void setPrice(int price) {
+		super.setPrice(price);
+	}
+	
+	public int getCalorie() {
+		return super.getCalorie();
+	}
+	
+	public void setCalorie(int calorie) {
+		super.setCalorie(calorie);
+	}
+	
+	public String getCountrOfOrigin() {
+		return super.getCountryOfOrigin();
+	}
+	
+	public void setCountrOfOrigin(String countrOfOrigin) {
+		super.setCountryOfOrigin(countrOfOrigin);
+	}
+
+	@Override
+	public String toString() {
+		return "Food [foodName=" + super.getFoodName() + ", price=" + super.getPrice() + ", calorie=" + super.getCalorie() + ", countryOfOrigin="
+				+ super.getCountryOfOrigin() + "]";
+	}
 
 }
