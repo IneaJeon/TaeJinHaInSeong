5713a2957a41470207166019b8d83387636f4b86
김태훈 <xogns603@hanmail.net>
2021-01-27 18:56:05 +0900

    20210127_김태훈
    OrderManager클래스 메소드생성 및 코드 추가
    JapaneseFood클래스 메소드생성

--- src/team/tjhis/order/JapaneseFood.java
+++ src/team/tjhis/order/JapaneseFood.java
@@ -1,5 +1,15 @@
 package team.tjhis.order;
 
-public class JapaneseFood {
+public class JapaneseFood extends Food {
+	
+	public JapaneseFood() {
+		super();
+	}
+	
+	
+	public void orderJapaneseFood() {
+		
+		
+	}
 
 }
--- src/team/tjhis/order/OrderManager.java
+++ src/team/tjhis/order/OrderManager.java
@@ -1,5 +1,57 @@
 package team.tjhis.order;
 
+import java.util.Scanner;
+
 public class OrderManager {
+
+	public void order() {
+
+		Scanner sc = new Scanner(System.in);
+
+		KoreanFood koreanFood = new KoreanFood();
+		SchoolFood schoolFood = new SchoolFood();
+		JapaneseFood japaneseFood = new JapaneseFood();
+		ChineseFood chineseFood = new ChineseFood();
+		FastFood fastFood = new FastFood();
+		CartManager cartManager = new CartManager();
+
+		System.out.println("====== 주문하기 ======");
+		System.out.println("1. 한식");
+		System.out.println("2. 분식");
+		System.out.println("3. 일식");
+		System.out.println("4. 중식");
+		System.out.println("5. 패스트푸드");
+		System.out.println("6. 장바구니");
+		System.out.println("9. 주문하기 종료");
+		int choice = sc.nextInt();
+
+		sc.nextLine();
+
+		switch (choice) {
+		case 1:
+			koreanFood.orderKoreanFood();
+			break;
+		case 2:
+//			schoolFood.orderSchoolFood();
+			break;
+		case 3:
+			japaneseFood.orderJapaneseFood();
+			break;
+		case 4:
+//			chineseFood.orderChineseFood();
+			break;
+		case 5:
+//			fastFood.orderFastFood();
+			break;
+		case 6:
+//			cartManager.?????();
+		case 9:
+			System.out.println("주문하기 종료");
+			return;
+		default:
+			System.out.println("번호를 잘못 입력하셨습니다.");
+			break;
+		}
+	}
 
 }
