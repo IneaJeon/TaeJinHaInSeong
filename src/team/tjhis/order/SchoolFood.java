33887cf884d57c302072a7b7aad0022f4d1e1ec0
good321ok@naver.com <good321ok@naver.com>
2021-01-27 18:43:14 +0900

    20210127_김성배
    실수로 KoreanFood클래스에 작성함.
    KoreanFood는 패치받은걸로 바꾸고 SchoolFood로 코드 옮김.

--- src/team/tjhis/order/SchoolFood.java
+++ src/team/tjhis/order/SchoolFood.java
@@ -1,5 +1,49 @@
 package team.tjhis.order;
 
-public class SchoolFood {
+public class SchoolFood extends Food {
 
+	public SchoolFood() {}
+
+	public SchoolFood(String foodName, int price, int calorie, String countryOfOrigin) {
+		super(foodName, price, calorie, countryOfOrigin);
+	}
+
+	public String getFoodName() {
+		return super.getFoodName();
+	}
+
+	public void setFoodName(String foodName) {
+		super.setFoodName(foodName);
+	}
+
+	public int getPrice() {
+		return super.getPrice();
+	}
+
+	public void setPrice(int price) {
+		super.setPrice(price);
+	}
+
+	public int getCalorie() {
+		return super.getCalorie();
+	}
+
+	public void setCalorie(int calorie) {
+		super.setCalorie(calorie);
+	}
+
+	public String getCountrOfOrigin() {
+		return super.getCountryOfOrigin();
+	}
+
+	public void setCountrOfOrigin(String countrOfOrigin) {
+		super.setCountryOfOrigin(countrOfOrigin);
+	}
+
+	@Override
+	public String toString() {
+		return "Food [foodName=" + super.getFoodName() + ", price=" + super.getPrice() + ", calorie=" + super.getCalorie() + ", countryOfOrigin="
+				+ super.getCountryOfOrigin() + "]";
+	}
+	
 }
