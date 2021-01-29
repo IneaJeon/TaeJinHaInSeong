4cf6bab81e346f075bd915f723bc0139595c3a66
HY <hymin1224@naver.com>
2021-01-30 04:02:36 +0900

    20210130_민하영
    KoreanFood 완성
    성배님 toString 위치대로 매개변수 있는 생성자 위치 수정해주면 kf.add 부분 주석 풀 예정.
    이전 페이지로 돌아가기도 팀원들과 상의 후 기능 추가하면 사용 예정.

--- src/team/tjhis/order/KoreanFood.java
+++ src/team/tjhis/order/KoreanFood.java
@@ -1,27 +1,119 @@
 package team.tjhis.order;
 
-public class KoreanFood extends Food{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-	/* 기본 생성자 생성 */
-	public KoreanFood() {
-		super();
+public class KoreanFood{
+
+	public List<Food> orderKoreanFood(List<Food> cartList) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<Food> kf = new ArrayList<>();
+		
+//		kf.add(new Food(1, "돼지 김치찌개", 8000, 465, "국내산"));
+//		kf.add(new Food(2, "해물 된장찌개", 8000, 183, "국내산"));
+//		kf.add(new Food(3, "해물 순두부찌개", 8000, 302, "국내산"));
+		
+		System.out.println("__________ 1인가득밥상 __________");
+		System.out.println("[ MENU ]");
+		
+		for(Food food : kf) {
+			System.out.println(food);
+			cartList.add(food);
+		}
+			
+		System.out.println("[ 9. 장바구니 가기 ]");
+//		System.out.println("[ 0. 이전 페이지로 돌아가기 ]");
+		System.out.println("______________________________");
+			
+		char answer = ' ';
+		int choice = 0;
+		int[] count = new int[3];
+		
+		for(int i = 0 ; i < 3 ; i++) {
+			count[i] = 1;
+		}
+		
+		while(true) {
+				
+			System.out.print("메뉴를 선택해 주세요 : ");
+
+			choice = sc.nextInt();
+			sc.nextLine();
+			
+			switch(choice) {
+			
+				case 1 :
+					System.out.print("돼지 김치찌개를 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("돼지 김치찌개를 장바구니에 담았습니다.");
+						cartList.get(0).setCount(count[0]++);
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					
+					break;
+					
+				case 2 : 
+					System.out.print("해물 된장찌개를 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("해물 된장찌개를 장바구니에 담았습니다.");
+						cartList.get(1).setCount(count[1]++);
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					
+					break;
+					
+				case 3 : 
+					System.out.print("해물 순두부찌개를 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("해물 순두부찌개를 장바구니에 담았습니다.");
+						cartList.get(2).setCount(count[2]++);
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					break;
+					
+				case 9 : 
+//					cartList = cartManager.cart(cartList);
+					break;
+				
+//				case 0 : 
+//					System.out.print("장바구니에 담긴 음식이 모두 삭제됩니다. 그래도 돌아가시겠습니까? (y/n) : ");
+//					char answer = sc.nextLine().charAt(0);
+//				
+//					if(answer == 'y' || answer == 'Y') {
+//						cartList.clear();
+//						return cartList;
+//					
+//					}else {
+//						break;
+//					}
+					
+				default : 
+					System.out.println("메뉴를 잘못 선택하셨습니다.");
+					break;
+				
+			}
+		
+		}
+		
 		
 	}
-	
-	public void orderKoreanFood() {
-		
-		System.out.println("__________ 가게 명 __________");
-		System.out.println();
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
