f55fc12c2bf709613e0f7474c3e522beee7b6b43
JH <yoojh0219@gmail.com>
2021-01-30 08:19:10 +0900

    20210130_유진희
    ChineseFood 및 CartManager 작성

--- src/team/tjhis/order/CartManager.java
+++ src/team/tjhis/order/CartManager.java
@@ -1,10 +1,109 @@
 package team.tjhis.order;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class CartManager {
 
 	public List<Food> cart(List<Food> cartList){
-		return cartList;
+		
+		int totalPrice = 0;		// 총 주문금액 넣을 변수
+		boolean flag = true;
+		int choice = 0;
+		char answer = ' ';
+		int cnt = 0;
+		int[] foodNo = new int[cartList.size()];
+		int index = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		UpdateCart updateCart = new UpdateCart();
+		
+		while(true) {
+			
+			System.out.println();
+			System.out.println("__________ 장바구니 목록 __________");
+			System.out.println();
+			
+			cnt = 0;
+			totalPrice = 0;
+			flag = true;
+			// 주문 목록 출력
+			for(Food food : cartList) {
+				
+				// 주문 수량이 0일 경우 출력하지 않음
+				if(food.getCount() == 0)
+					continue;
+				
+				System.out.println("이름 : " + food.getFoodName() + ", 수량 : " + food.getCount() + ", 가격 : " + food.getPrice()*food.getCount());
+				totalPrice += food.getPrice()*food.getCount();
+				foodNo[cnt++] = food.getFoodNo();
+				flag = false;	// 장바구니에 담은 메뉴가 있을 경우 flag를 false로 변경
+			}
+			
+			if(flag) {	// 장바구니가 비어 있는 경우
+				System.out.println("장바구니에 담으신 메뉴가 없습니다.");
+				System.out.println("________________________________");
+				System.out.println();
+				System.out.println("이전 페이지로 돌아갑니다.");
+				System.out.println();
+				return cartList;
+			}
+			
+			System.out.println();
+			System.out.println("총 주문금액 : " + totalPrice + "원");
+			System.out.println("________________________________");
+			System.out.println();
+			System.out.println("[ 1. 더 담으러 가기 ]");
+			int no = 2;
+			for(int i = 0 ; i < cnt ; i++) {
+				System.out.println("[ " + no++ + ". " + cartList.get(foodNo[i]-1).getFoodName() + " - ]");
+				System.out.println("[ " + no++ + ". " + cartList.get(foodNo[i]-1).getFoodName() + " + ]");
+				System.out.println("[ " + no++ + ". " + cartList.get(foodNo[i]-1).getFoodName() + " 삭제 ]");
+			}
+			System.out.println("[ 0. 주문하기 ]");
+
+			System.out.println();
+			System.out.print("원하시는 항목을 선택해 주세요 : ");
+			
+			choice = sc.nextInt();
+			sc.nextLine();
+			
+			if(2 <= choice && choice < no) {
+				
+				index = foodNo[(choice-2) / 3] - 1;
+				
+				switch(choice % 3) {			
+//					case 0 : cartList = updateCart.plusMenu(cartList, index); break;
+//					case 1 : cartList = updateCart.deleteMenu(cartList, index); break;
+//					case 2 : cartList = updateCart.minusMenu(cartList, index);
+				}
+			}else {
+				
+				switch(choice) {
+					case 1 : return cartList;
+					
+					case 0 : 
+						System.out.print("장바구니에 담긴 음식을 주문하시겠습니까?(y/n) : ");
+						answer = sc.nextLine().charAt(0);
+						System.out.println();
+						
+						if(answer == 'Y' || answer == 'y') {
+							System.out.println("주문이 완료되었습니다!");
+							System.out.println("배달 예상 시간은 " + ((int)(Math.random() * 10) + 30) + "분입니다.");
+							System.out.println();
+							cartList.clear();
+							return cartList;
+						}else if(answer == 'N' || answer == 'n') {
+							System.out.println("취소되었습니다.");
+							break;
+						}
+						// gui로 구현 예정이므로 예외처리 x
+						
+					default : System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.");
+				}
+			}
+
+		
+		}//while
 	}
 }
--- src/team/tjhis/order/ChineseFood.java
+++ src/team/tjhis/order/ChineseFood.java
@@ -1,39 +1,126 @@
 package team.tjhis.order;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ChineseFood {
 
-	public List<Food> orderChineseFood(List<Food> cartList) {
+	public List<Food> orderChineseFood(List<Food> cartList, CartManager cartManager) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		// 중국음식 메뉴 목록 담을 리스트 생성
+		List<Food> cf = new ArrayList<>();
+		
+		// 메뉴 목록 어레이리스트에 추가
+//		cf.add(new Food(1, "짜장면", 6000, 864, "국내산"));
+//		cf.add(new Food(2, "짬뽕", 8000, 788, "국내산"));
+//		cf.add(new Food(3, "탕수육", 15000, 450, "국내산"));
+
+		printMenu(cf);	// 메뉴 출력
+		
+		// 장바구니 목록에 메뉴 목록 미리 담아두기
+		for(Food food : cf) {
+			cartList.add(food);	
+		}
+
+		// 각각의 메뉴 수량 카운트할 변수를 배열로 선언 및 각 인덱스 값을 1로 초기화
+		int[] count = new int[cartList.size()];
+		for(int i = 0 ; i < cartList.size() ; i++) {
+			count[i] = 1;
+		}
+
+		int choice = 0;	// 선택 메뉴 저장
+		int index = 0;	// cartList의 인덱스
+		String name = "";
+		char answer = ' ';
+		boolean flag = true;
+		
+		while(true) {
+			
+			System.out.print("메뉴를 선택해 주세요 : ");
+			
+			choice = sc.nextInt();
+			sc.nextLine();	// 버퍼 제거
+			index = choice - 1;	// 인덱스 계산(메뉴 번호는 1부터 시작, index는 0부터 시작)
+			
+		/* 음식을 선택한 경우 */
+			if(0 < choice && choice <= cartList.size())	{ 
+				
+				name = cartList.get(index).getFoodName();	// 선택한 음식의 이름 name에 저장
+				
+				System.out.print(name + "을 장바구니에 담으시겠습니까?(y/n) : ");
+				answer = sc.nextLine().charAt(0);
+				
+				if(answer == 'Y' || answer == 'y') {
+				
+					// 선택한 음식 인스턴스의 count변수에 setter를 이용하여 값 대입 및 카운트 1 증가
+					cartList.get(index).setCount(count[index]++);
+					System.out.println(name + "을 장바구니에 담았습니다.");
+					System.out.println();
+					flag = false;
+				
+				}else if(answer == 'N' || answer == 'n') {
+					
+					System.out.println("취소되었습니다.");
+					System.out.println();
+				}	// 버튼 2개 구현 및 버튼 클릭 시에만 실행 예정이라 else는 작성 x
+		
+				
+		/* 장바구니로 이동을 선택한 경우 */
+			}else if(choice == 9) {
+				cartList = cartManager.cart(cartList);
+				if(cartList.size() == 0) return cartList;	// 주문 완료된 경우
+				printMenu(cf);	// 더 담을 경우
+			
+//		/* 이전 페이지로 돌아가기를 선택한 경우(추후 구현) */
+//			}else if(choice == 0) {
+//				if(flag) {
+//					System.out.println("이전 페이지로 돌아갑니다.");
+//					System.out.println();
+//					return cartList;
+//				}
+//			
+//				System.out.print("장바구니에 담긴 음식이 모두 삭제됩니다. 그래도 돌아가시겠습니까?(y/n) : ");
+//				answer = sc.nextLine().charAt(0);
+//				
+//				if(answer == 'y' || answer == 'y') {						
+//					cartList.clear();
+//					System.out.println("이전 페이지로 돌아갑니다.");
+//					System.out.println();
+//					return cartList;
+//					
+//				}else if(answer == 'N' || answer == 'n') {
+//					System.out.println("취소되었습니다.");
+//					System.out.println();
+//				}
+				
+		/* 예외 처리 */
+			}else {
+				System.out.println("메뉴를 잘못 선택하셨습니다. 다시 선택해 주세요.");
+			}
+	
+		}
+		
+	}
+	
+	public void printMenu(List<Food> cf) {
 		
 		System.out.println();
 		System.out.println("__________강남반점__________");
 		System.out.println();
-		System.out.println("[ 1. 짜장면 ]");
-		System.out.println("[ 2. 짬뽕 ]");
-		System.out.println("[ 3. 탕수육 ]");
-		System.out.println("[ 9. 돌아가기 ]");
+		System.out.println("[ MENU ]");
+		System.out.println();
+		
+		for(Food food : cf) {
+			System.out.println(food);
+		}
+		
+		System.out.println();
+		System.out.println("9. 장바구니로 이동");
+		System.out.println("0. 이전 페이지로 돌아가기");
 		System.out.println("__________________________");
 		System.out.println();
-		System.out.print("메뉴를 선택해 주세요 : ");
-		int choice = sc.nextInt();
-		sc.nextLine();
-		
-		switch(choice) {
-		
-			case 1 : break;
-			case 2 : break;
-			case 3 : break;
-			case 9 : break;
-			default : 
-			
-		}
-	
-	
-		return cartList;
-	
 	}
 }
