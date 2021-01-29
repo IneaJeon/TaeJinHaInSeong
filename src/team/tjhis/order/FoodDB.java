2154b50ffcc64c203e1658f9634e4bb18c632e54
good321ok@naver.com <good321ok@naver.com>
2021-01-29 09:43:55 +0900

    20210129_김성배
    분식에 주문하기 추가
    FoodDTO삭제
    FoodDB추가
    Cart추가

---
+++ src/team/tjhis/order/FoodDB.java
@@ -0,0 +1,7 @@
+package team.tjhis.order;
+
+public class FoodDB {
+
+	
+	
+}
--- src/team/tjhis/order/SchoolFood.java
+++ src/team/tjhis/order/SchoolFood.java
@@ -1,6 +1,11 @@
 package team.tjhis.order;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SchoolFood extends Food {
+	
+	private ArrayList<Food> foodList = new ArrayList<>();
 
 	public SchoolFood() {}
 
@@ -8,42 +13,35 @@ public class SchoolFood extends Food {
 		super(foodName, price, calorie, countryOfOrigin);
 	}
 
-	public String getFoodName() {
-		return super.getFoodName();
-	}
-
-	public void setFoodName(String foodName) {
-		super.setFoodName(foodName);
-	}
-
-	public int getPrice() {
-		return super.getPrice();
-	}
-
-	public void setPrice(int price) {
-		super.setPrice(price);
-	}
-
-	public int getCalorie() {
-		return super.getCalorie();
-	}
-
-	public void setCalorie(int calorie) {
-		super.setCalorie(calorie);
-	}
-
-	public String getCountrOfOrigin() {
-		return super.getCountryOfOrigin();
-	}
-
-	public void setCountrOfOrigin(String countrOfOrigin) {
-		super.setCountryOfOrigin(countrOfOrigin);
-	}
-
-	@Override
-	public String toString() {
-		return "Food [foodName=" + super.getFoodName() + ", price=" + super.getPrice() + ", calorie=" + super.getCalorie() + ", countryOfOrigin="
-				+ super.getCountryOfOrigin() + "]";
+	public void orderSchoolFood(FoodDB foodDB, Cart cart ) {
+		
+		foodList.clear(); // 메소드 호출하면 foodList안의 요소들을 제거하고 시작
+		int choice;
+		Scanner sc = new Scanner(System.in);
+//		for(int i = 0 ; i < foodDB.getDb().size() ; i++) {
+//			if(foodDB.getDb().get(i) instanceof SchoolFood) { // 자료형이 SchoolFood인 음식들을 데이터베이스에서 찾아서
+//				foodList.add(foodDB.getDb().get(i)); // foodList에 저장한다.
+//			}
+//		}
+		
+		System.out.println("__________ 음식 리스트 __________");
+		for(int i = 0 ; i < foodList.size() ; i++) {
+			System.out.println((i + 1) + ". " + foodList.get(i)); // foodList에 저장해놓은 값으로 메뉴 출력
+		}
+		System.out.println("0. 카테고리 선택 페이지로 돌아가기");
+		
+		choice = sc.nextInt(); // 번호 선택
+		sc.nextLine(); // 버퍼 제거
+		if(choice == 0) { // 0번 선택시 되돌아가기
+			return;
+		} else if(choice > 0 && choice <= foodList.size()) { // 1 ~ 메뉴 마지막번호 선택시
+//			cart.getCart().add(foodList.get(choice - 1)); // 카트에 해당 음식 담기
+			System.out.println(foodList.get(choice - 1).getFoodName() + "을(를) 장바구니에 담았습니다."); // 담은 음식 이름 출력
+		} else {
+			System.out.println("잘못 입력 하셨습니다.");
+			System.out.println("다시 입력해 주세요.");
+		}
+		
 	}
 	
 }
