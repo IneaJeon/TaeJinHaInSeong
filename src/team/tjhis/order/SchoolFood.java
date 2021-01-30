1e2b8abe09d4b9c34bb53f5c5d83341b3b04ae56
good321ok@naver.com <good321ok@naver.com>
2021-01-30 11:10:29 +0900

    20210130_김성배
    분식 카테고리 수정

--- src/team/tjhis/order/SchoolFood.java
+++ src/team/tjhis/order/SchoolFood.java
@@ -1,47 +1,112 @@
 package team.tjhis.order;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class SchoolFood extends Food {
+public class SchoolFood {
 	
-	private ArrayList<Food> foodList = new ArrayList<>();
-
-	public SchoolFood() {}
-
-	public SchoolFood(String foodName, int price, int calorie, String countryOfOrigin) {
-//		super(foodName, price, calorie, countryOfOrigin);
-	}
-
-	public void orderSchoolFood(FoodDB foodDB, Cart cart ) {
+	public void orderKoreanFood(List<Food> cartList) {
 		
-		foodList.clear(); // 메소드 호출하면 foodList안의 요소들을 제거하고 시작
-		int choice;
+		CartManager cm = new CartManager();
 		Scanner sc = new Scanner(System.in);
-//		for(int i = 0 ; i < foodDB.getDb().size() ; i++) {
-//			if(foodDB.getDb().get(i) instanceof SchoolFood) { // 자료형이 SchoolFood인 음식들을 데이터베이스에서 찾아서
-//				foodList.add(foodDB.getDb().get(i)); // foodList에 저장한다.
-//			}
-//		}
 		
-		System.out.println("__________ 음식 리스트 __________");
-		for(int i = 0 ; i < foodList.size() ; i++) {
-			System.out.println((i + 1) + ". " + foodList.get(i)); // foodList에 저장해놓은 값으로 메뉴 출력
+		List<Food> sf = new ArrayList<>();
+		
+		sf.add(new Food(1, "떡볶이", 3500, 352, "호주산"));
+		sf.add(new Food(2, "찰순대", 4000, 513, "국내산"));
+		sf.add(new Food(3, "모듬튀김", 4500, 380, "중국산"));
+		
+		System.out.println("_______________________________");
+		System.out.println("[ MENU ]");
+		
+		for(Food food : sf) {
+			System.out.println(food);
+			cartList.add(food);
 		}
-		System.out.println("0. 카테고리 선택 페이지로 돌아가기");
+			
+		System.out.println("[ 9. 장바구니 가기 ]");
+		System.out.println("[ 0. 이전 페이지로 돌아가기 ]");
+		System.out.println("______________________________");
+			
+		char answer = ' ';
+		int choice = 0;
 		
-		choice = sc.nextInt(); // 번호 선택
-		sc.nextLine(); // 버퍼 제거
-		if(choice == 0) { // 0번 선택시 되돌아가기
-			return;
-		} else if(choice > 0 && choice <= foodList.size()) { // 1 ~ 메뉴 마지막번호 선택시
-//			cart.getCart().add(foodList.get(choice - 1)); // 카트에 해당 음식 담기
-			System.out.println(foodList.get(choice - 1).getFoodName() + "을(를) 장바구니에 담았습니다."); // 담은 음식 이름 출력
-		} else {
-			System.out.println("잘못 입력 하셨습니다.");
-			System.out.println("다시 입력해 주세요.");
+		while(true) {
+				
+			System.out.print("메뉴를 선택해 주세요 : ");
+
+			choice = sc.nextInt();
+			sc.nextLine();
+			
+			switch(choice) {
+			
+				case 1 :
+					System.out.print("떡볶이를 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("떡볶이를 장바구니에 담았습니다.");
+						cartList.get(0).setCount(cartList.get(0).getCount() + 1);
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					
+					break;
+					
+				case 2 : 
+					System.out.print("찰순대를 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("찰순대를 장바구니에 담았습니다.");
+						cartList.get(1).setCount(cartList.get(1).getCount() + 1);
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					
+					break;
+					
+				case 3 : 
+					System.out.print("모듬튀김을 장바구니에 담으시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+					
+					if(answer == 'y' || answer == 'Y') {
+						System.out.println("모듬튀김을 장바구니에 담았습니다.");
+						cartList.get(2).setCount(cartList.get(2).getCount() + 1);
+						
+						
+					}else if(answer == 'n' || answer == 'N') {
+						System.out.println("취소되었습니다.");
+						
+					}
+					break;
+					
+				case 9 : 
+					cm.cart(cartList);
+					break;
+				
+				case 0 : 
+					System.out.print("장바구니에 담긴 음식이 모두 삭제됩니다. 그래도 돌아가시겠습니까? (y/n) : ");
+					answer = sc.nextLine().charAt(0);
+				
+					if(answer == 'y' || answer == 'Y') {
+						cartList.clear();
+						return;
+					}
+					break;
+					
+				default : 
+					System.out.println("메뉴를 잘못 선택하셨습니다.");
+				
+			}
+		
 		}
-		
+	
 	}
 	
 }
