e7f049e4853ff024fba033f7fcaf5d3480763a24
good321ok@naver.com <good321ok@naver.com>
2021-02-03 16:30:07 +0900

    20210203_김성배
    member패키지의 FindPwd클래스 입출력 적용 및 팝업창 메세지 비움

--- src/team/tjhis/member/FindPwd.java
+++ src/team/tjhis/member/FindPwd.java
@@ -1,33 +1,84 @@
 package team.tjhis.member;
 
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Scanner;
 
 public class FindPwd {
 
 	// 비밀번호 찾기 메소드
-	public void findPwd(MemberDB db) {
-		if(db.memberDB.size() == 0) {
-			System.out.println("회원 데이터베이스가 비어있습니다!");
-			System.out.println("회원가입을 먼저 진행해주세요.");
-			return;
-		}
-		Scanner sc = new Scanner(System.in);
-		System.out.print("이름 : ");
-		String name = sc.nextLine();
-		System.out.print("아이디 : ");
-		String id = sc.nextLine();
-		System.out.print("핸드폰 번호 : ");
-		String phoneNum = sc.nextLine();
-		for(int i = 0 ; i < db.memberDB.size() ; i++) { // 인덱스를 1씩 증가시키며 해당 인덱스 안의 인스턴스 필드멤버들이 입력한 값과 일치하는 인스턴스가 존재하면 if안의 내용 실행
-			if(db.memberDB.get(i).getName().equals(name) && db.memberDB.get(i).getId().equals(id) && db.memberDB.get(i).getPhoneNum().equals(phoneNum)) {
-				System.out.println("찾으시는 비밀번호는 [" + db.memberDB.get(i).getPwd() + "] 입니다.");
+		public void findPwd(MemberDB db) {
+			if(!new File("src/team/tjhis/member/members.txt").exists()) {
+				System.out.println("회원 데이터베이스가 비어있습니다!");
+				System.out.println("회원가입을 먼저 진행해주세요.");
 				return;
 			}
+			Scanner sc = new Scanner(System.in);
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+			System.out.print("아이디 : ");
+			String id = sc.nextLine();
+			System.out.print("핸드폰 번호 : ");
+			String phoneNum = sc.nextLine();
+			
+			ObjectInputStream objIn = null;
+			MemberDTO temp = null;
+			
+			try {
+				
+				objIn = new ObjectInputStream(new BufferedInputStream(new FileInputStream("src/team/tjhis/member/members.txt")));
+				
+				while(true) {
+					
+					temp = (MemberDTO)objIn.readObject();
+					if(temp.getName().equals(name) && temp.getId().equals(id) && temp.getPhoneNum().equals(phoneNum)) {
+						
+						System.out.println("찾으시는 비밀번호는 [" + temp.getPwd() + "] 입니다.");
+						break;
+						
+					}
+					
+				}
+				
+			} catch (EOFException e) {
+				
+				System.out.println("입력하신 정보와 일치하는 회원이 존재하지 않습니다!");
+				
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+				
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+				
+			} catch (ClassNotFoundException e) {
+				
+				e.printStackTrace();
+				
+			} finally {
+				
+				if(objIn != null) {
+					
+					try {
+						
+						objIn.close();
+						
+					} catch (IOException e) {
+						
+						e.printStackTrace();
+						
+					}
+					
+				}
+				
+			}
+			
 		}
-		// 일치하는 인스턴스가 존재하지 않아 그대로 for문을 탈출했을때
-		System.out.println("해당하는 회원의 정보가 존재하지 않습니다.");
-		return;
-		
-	}
 	
 }
