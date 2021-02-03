7ff123c0a4f5a30bd22cb5bea4ac40cd352c585d
김태훈 <xogns603@hanmail.net>
2021-02-04 03:00:30 +0900

    20210204_김태훈
    수정

--- src/team/tjhis/gui/FindIdPage.java
+++ src/team/tjhis/gui/FindIdPage.java
@@ -1,9 +1,12 @@
 package team.tjhis.gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.BufferedInputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
@@ -42,26 +45,62 @@ public class FindIdPage extends JPanel {
 
 		JLabel lb = new JLabel(new ImageIcon(id));
 		lb.setSize(1440, 790);
+		
+		Font font = new Font("돋움", Font.PLAIN, 17);
 
 		JTextField nameTf = new JTextField(20);
 		nameTf.setSize(336,40);
-		nameTf.setLocation(554, 299);
+		nameTf.setLocation(554, 324);
+		nameTf.setFont(font);
+		nameTf.setText("이름");
 		nameTf.setBorder(null);
 		
 		JTextField phoneNumTf = new JTextField(20);
 		phoneNumTf.setSize(336,40);
-		phoneNumTf.setLocation(554, 424);
+		phoneNumTf.setLocation(554, 403);
+		phoneNumTf.setFont(font);
+		phoneNumTf.setText("휴대폰");
 		phoneNumTf.setBorder(null);
 		
 		JButton btn = new JButton("확인");
-		btn.setSize(205, 50);
-		btn.setLocation(620, 602);
+		btn.setSize(350, 50);
+		btn.setLocation(545, 575);
 		btn.setOpaque(false);
 
 		this.add(lb);
 		this.add(nameTf);
 		this.add(phoneNumTf);
 		this.add(btn);
+		
+		nameTf.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (nameTf.getText().equals("이름")) {
+
+					nameTf.setText("");
+
+				}
+
+			}
+
+		});
+
+		phoneNumTf.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (phoneNumTf.getText().equals("휴대폰")) {
+
+					phoneNumTf.setText("");
+
+				}
+
+			}
+
+		});
 		
 		btn.addActionListener(new ActionListener() {
 			ObjectInputStream objIn = null;
