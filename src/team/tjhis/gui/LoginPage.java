119db8502d68ef893dbaec6f7761a7df23ff0b0e
winze <winze@INEA>
2021-02-04 03:24:23 +0900

    20210204_전인애
    로그인
    회원가입
    이미지(로그인)

--- src/team/tjhis/gui/LoginPage.java
+++ src/team/tjhis/gui/LoginPage.java
@@ -1,14 +1,20 @@
 package team.tjhis.gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 public class LoginPage extends JPanel {
 
@@ -28,59 +34,56 @@ public class LoginPage extends JPanel {
 		this.setBounds(0, 120, 1440, 790);
 		this.setBackground(Color.white);
 		
-		/*폰트 컬러기본설정*/
-		
-		Image loginimg = new ImageIcon("images/login.PNG").getImage().getScaledInstance(1440, 790, 0);
+		Image loginimg = new ImageIcon("images/login1.PNG").getImage().getScaledInstance(1440, 790, 0);
 		
 		JLabel logoLabel = new JLabel(new ImageIcon(loginimg));
 		logoLabel.setBounds(0, 0, 1440, 790);
 
+
 		/* 아이디 필드 */
-		JTextField idField = new JTextField();
-		idField.setSize(400, 64);
-		idField.setLocation(520, 185);
-		idField.setOpaque(false);
+		JTextField idF = new JTextField(10);
+		idF.setText("아이디");
+		idF.setSize(336,40);
+		idF.setLocation(525, 200);
+		idF.setBorder(null);
 
 		/* 비밀번호 필드 */
-		JTextField pwdField = new JTextField();
-		pwdField.setSize(400, 64);
-		pwdField.setLocation(520, 255);
-		pwdField.setOpaque(false);
+		JTextField pwdF= new JTextField(10);
+		pwdF.setText("비밀번호");
+		pwdF.setSize(336,40);
+		pwdF.setLocation(525, 270);
+		pwdF.setBorder(null);
 		
 		/* 로그인 버튼 */
 		JButton loginbtn = new JButton();
 		loginbtn.setSize(400, 64);
-		loginbtn.setLocation(520, 388);
+		loginbtn.setLocation(525, 357);
 		loginbtn.setOpaque(false);
 		
 		/* 아이디찾기 버튼 */
 		JButton idBtn = new JButton();
 		idBtn.setSize(400, 64);
-		idBtn.setLocation(520, 457);
+		idBtn.setLocation(525, 457);
 		idBtn.setOpaque(false);
 		
 		/* 비밀번호찾기 버튼 */
 		JButton pwdBtn = new JButton();
 		pwdBtn.setSize(400, 64);
-		pwdBtn.setLocation(520, 528);
+		pwdBtn.setLocation(525, 528);
 		pwdBtn.setOpaque(false);
 		
-		loginPage.add(logoLabel);
-		loginPage.add(idField);
-		loginPage.add(pwdField);
-		
-		loginPage.add(loginbtn);
-		loginPage.add(idBtn);
-		loginPage.add(pwdBtn);
-		
-		mf.add(loginPage);
-		
-		/* 클릭 시 mainPage로 이동 */
+		this.add(idF);
+		this.add(pwdF);
+		this.add(logoLabel);
+		this.add(loginbtn);
+		this.add(idBtn);
+		this.add(pwdBtn);
+	
+		/* 클릭 시 해당페이지 이동 */
 		loginbtn.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
 				ChangePage.changePanel(mf, loginPage, new MainPage(mf));
 			}
 		});
@@ -89,7 +92,7 @@ public class LoginPage extends JPanel {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				ChangePage.changePanel(mf, loginPage, new FindIdPage());				
+				ChangePage.changePanel(mf, loginPage, new FindIdPage(mf, null));				
 			}
 		});
 		
@@ -101,6 +104,27 @@ public class LoginPage extends JPanel {
 			}
 		});
 		
+		idF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (idF.getText().equals("아이디")) {
+					idF.setText("");
+				}
+			}
+		});
+
+		pwdF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (pwdF.getText().equals("비밀번호")) {
+					pwdF.setText("");
+				}
+			}
+		});
+
+
 	}
 	
 }
--- src/team/tjhis/gui/SignUpPage.java
+++ src/team/tjhis/gui/SignUpPage.java
@@ -4,97 +4,167 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-public class SignUpPage extends JPanel{
-	
+
+public class SignUpPage extends JPanel {
+
 	private MainFrame mf;
 	private JPanel signUpPage;
 
 	/* 기본생성자 */
-	public SignUpPage() {}
+	public SignUpPage() {
+	}
 
 	public SignUpPage(MainFrame mf) {
-		
+
 		this.mf = mf;
 		this.signUpPage = this;
-		
+
 		this.setLayout(null);
 		this.setBounds(0, 120, 1440, 790);
-		this.setBackground(Color.white);
-		
-		
 		/* 회원가입 */
 		Image signUpImg = new ImageIcon("images/signUp.PNG").getImage().getScaledInstance(1440, 790, 0);
-		
+
 		JLabel logoLabel = new JLabel(new ImageIcon(signUpImg));
 		logoLabel.setBounds(0, 0, 1440, 790);
-		
-		
-		/*이름 필드*/
+
+		/* 이름 필드 */
 		JTextField nameF = new JTextField(20);
-		nameF.setSize(336,40);
-		nameF.setLocation(554, 200);
 		nameF.setBorder(null);
-		
+		nameF.setText("이름");
+		nameF.setSize(336, 40);
+		nameF.setLocation(554, 200);
 		/* id 필드 */
 		JTextField idF = new JTextField(20);
-		idF.setSize(336,40);
+		idF.setText("아이디");
+		idF.setSize(336, 40);
 		idF.setLocation(554, 260);
 		idF.setBorder(null);
-		
+
 		/* 비번 필드 */
 		JTextField pwdF = new JTextField(20);
-		pwdF.setSize(336,40);
+		pwdF.setText("비밀번호");
+		pwdF.setSize(336, 40);
 		pwdF.setLocation(554, 320);
 		pwdF.setBorder(null);
-		
+
 		/* 비번확인 필드 */
 		JTextField pwdDF = new JTextField(20);
-		pwdDF.setSize(336,40);
+		pwdDF.setText("비밀번호 확인");
+		pwdDF.setSize(336, 40);
 		pwdDF.setLocation(554, 380);
 		pwdDF.setBorder(null);
-		
+
 		/* 전화번호 필드 */
 		JTextField phonF = new JTextField(20);
-		phonF.setSize(336,40);
+		phonF.setText("전화번호");
+		phonF.setSize(336, 40);
 		phonF.setLocation(554, 440);
 		phonF.setBorder(null);
-		
+
 		/* 주소 필드 */
 		JTextField adrF = new JTextField(20);
-		adrF.setSize(336,40);
+		adrF.setText("주소");
+		adrF.setSize(336, 40);
 		adrF.setLocation(554, 500);
 		adrF.setBorder(null);
-		
+
 		/* 확인 필드 */
 		JButton signUpBtn = new JButton("확인");
 		signUpBtn.setSize(205, 50);
-		signUpBtn.setLocation(620, 590);
+		signUpBtn.setLocation(620, 595);
 		signUpBtn.setOpaque(false);
 
-		this.add(logoLabel);
 		this.add(nameF);
 		this.add(idF);
 		this.add(pwdF);
 		this.add(pwdDF);
 		this.add(phonF);
 		this.add(adrF);
+		this.add(logoLabel);
 		this.add(signUpBtn);
 
-
 		signUpBtn.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				ChangePage.changePanel(mf, signUpPage, new MainPage(mf));
 				PopUpPage.popUp(mf, "images/welcome.png");
 			}
 		});
+		nameF.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (nameF.getText().equals("이름")) {
+					nameF.setText("");
+				}
+			}
+		});
 
+		idF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (idF.getText().equals("아이디")) {
+					idF.setText("");
+				}
+			}
+		});
+
+		pwdF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (pwdF.getText().equals("비밀번호")) {
+					pwdF.setText("");
+				}
+			}
+		});
+
+
+		pwdDF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (pwdDF.getText().equals("비밀번호 확인")) {
+					pwdDF.setText("");
+				}
+			}
+		});
+		
+		phonF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (phonF.getText().equals("전화번호")) {
+					phonF.setText("");
+				}
+			}
+		});
+		
+		adrF.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (adrF.getText().equals("주소")) {
+					adrF.setText("");
+				}
+			}
+		});
 	}
 
 }
